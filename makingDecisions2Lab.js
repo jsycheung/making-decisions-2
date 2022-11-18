494f2f05a04f53d8f83b014e089a44cec9e226b9
Jasmine Cheung <jasminesyc2000@gmail.com>
2022-11-17 22:47:01 -0500

    Problem 5 solved

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -62,6 +62,17 @@ var score = 74;
 */
 
 //Code Here
+if (score >= 90) {
+  console.log("A");
+} else if (score >= 80 && score <= 89) {
+  console.log("B");
+} else if (score >= 70 && score <= 79) {
+  console.log("C");
+} else if (score >= 60 && score <= 69) {
+  console.log("D");
+} else {
+  console.log("F");
+}
 
 ////////// Intermediate Problems //////////
 
